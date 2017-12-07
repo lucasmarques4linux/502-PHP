06b858cb88f23d1f33b59936df6f890181069b0e
Lucas Marques <lucasmarques73@hotmail.com>
2017-12-06 22:10:06 -0200

    Apache

--- dexterescolaapi/Controller/ApiController/ApiController.php
+++ dexterescolaapi/Controller/ApiController/ApiController.php
@@ -9,25 +9,62 @@ class ApiController
 	public function run(){
 
 		$method = $_SERVER['REQUEST_METHOD'];
-		$route 	= $_GET['route'];
+		// $route 	= isset($_GET['route']) ? $_GET['route'] : 'home';
+
+		$route = $_SERVER['REQUEST_URI'];
+
+		if ($route != '/') {
+            $arrayRoute = explode('/', $route);
+        }
+
+        $route = isset($arrayRoute[1]) ? $arrayRoute[1] : 'home';
 
 		switch ($route) {
 			case 'alunos':
 				
-				switch ($method) {
-					case 'GET':
-						$id = isset($_GET['id']) ? $_GET['id'] : null;
-						if ($id) {
-							AlunoController::find($id);
-						} else {
-							AlunoController::all();
-						}
-						break;
-					
-					default:
-						# code...
-						break;
-				}
+					switch ($method) {
+						case 'GET':
+							$id = isset($_GET['id']) ? $_GET['id'] : null;
+							if ($id) {
+								AlunoController::find($id);
+							} else {
+								AlunoController::all();
+							}
+							break;
+						case 'POST':
+							echo "POST";
+							break;
+						case 'PUT':
+							echo "PUT";
+							break;
+						case 'DELETE':
+							echo "DELETE";
+							break;
+						default:
+							# code...
+							break;
+					}
+
+				break;
+			case 'home':
+			switch ($method) {
+				case 'GET':
+					echo "GET";
+					break;
+				case 'POST':
+					echo "POST";
+					break;
+				case 'PUT':
+					echo "PUT";
+					break;
+				case 'DELETE':
+					echo "DELETE";
+					break;
+				default:
+					# code...
+					break;
+			}
+				
 				break;			
 			default:
 				echo "Rota Inválida";
